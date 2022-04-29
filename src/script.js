0c300cd0b8b74d6f4114c63a9009a6b1e4272f15
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-04-29 18:20:44 +0300

    add data

---
+++ src/script.js
@@ -0,0 +1,17 @@
+const data = {
+    'javascript': {
+        'name': 'JavaScript',
+        'logo': 'img/javascript.png',
+        'description': 'JavaScript often abbreviated JS, is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS. Over 97% of websites use JavaScript on the client side for web page behavior, often incorporating third-party libraries. All major web browsers have a dedicated JavaScript engine to execute the code on users\' devices.'
+    },
+    'python': {
+        'name': 'Python',
+        'logo': 'img/python.png',
+        'description': 'Python is a high-level, interpreted, general-purpose programming language. Its design philosophy emphasizes code readability with the use of significant indentation. Its language constructs and object-oriented approach aim to help programmers write clear, logical code for small- and large-scale projects.'
+    },
+    'c++': {
+        'name': 'C++',
+        'logo': 'img/c++.png',
+        'description': 'C++ is a general-purpose programming language created by Bjarne Stroustrup as an extension of the C programming language, or "C with Classes". The language has expanded significantly over time, and modern C++ now has object-oriented, generic, and functional features in addition to facilities for low-level memory manipulation.'
+    }
+};
