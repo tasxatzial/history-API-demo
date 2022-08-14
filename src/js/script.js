81989a5b405a14df3ad9ae898a9b27db1dbc3adf
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-08-14 14:23:12 +0300

    minor refactoring, add function doc

--- src/js/script.js
+++ src/js/script.js
@@ -24,18 +24,24 @@ let main, mainText, header, h1;
 const nav = document.querySelector('.nav');
 const languageNavLinks = nav.querySelectorAll('.nav-lang');
 
+/* Initializes the page based on the initial URL */
 (function () {
     createMainContent();
     let url = window.location.href.split('/').pop();
     if (!url) {
         url = 'index.html';
     }
-    updateMainContent(url);
+    updatePage(url);
     window.addEventListener('popstate', popstate);
     window.history.replaceState(url, '', null);
     nav.addEventListener('click', clickNav);
 })();
 
+/**
+ * Initializes everything on the page except the navigation links.
+ * Called when:
+ * 1) the page loads for the first time.
+ */
 function createMainContent() {
     h1 = document.createElement('h1');
     h1.classList.add('main-title');
@@ -56,13 +62,25 @@ function createMainContent() {
     container.appendChild(main);
 }
 
-function updateMainContent(url) {
+/**
+ * Updates the page. Called when:
+ * 1) a navigation link is clicked.
+ * 2) the page loads for the first time.
+ * 3) the browser back/forward button is clicked.
+ */
+function updatePage(url) {
     h1.textContent = states[url].name;
     mainText.textContent = states[url].description;
     updateLanguageLogo(url);
-    updateSelectedLanguage(url);
+    updateSelectedNavLink(url);
 }
 
+/**
+ * Updates the language logo. Called when:
+ * 1) a navigation link is clicked.
+ * 2) the page loads for the first time.
+ * 3) the browser back/forward button is clicked.
+ */
 function updateLanguageLogo(url) {
     let languageLogo = header.querySelector('.language-logo');
     if (url === 'index.html') {
@@ -82,7 +100,13 @@ function updateLanguageLogo(url) {
     }
 }
 
-function updateSelectedLanguage(url) {
+/**
+ * Updates the selected navigation link. Called when:
+ * 1) a navigation link is clicked.
+ * 2) the page loads for the first time.
+ * 3) the browser back/forward button is clicked.
+ */
+function updateSelectedNavLink(url) {
     if (url === 'index.html') {
         for (let i = 0; i < languageNavLinks.length; i++) {
             languageNavLinks[i].classList.remove('selected');
@@ -98,6 +122,10 @@ function updateSelectedLanguage(url) {
     }
 }
 
+/**
+ * Updates the window title. Called when:
+ * 1) a navigation link is clicked.
+ */
 function updateWindowTitle(url) {
     if (url === 'index.html') {
         document.title = states['index.html'].name;
@@ -106,17 +134,25 @@ function updateWindowTitle(url) {
     }
 }
 
+/**
+ * Updates the page. Called when:
+ * 1) a navigation link is clicked.
+ */
 function clickNav(e) {
     if (e.target.classList.contains('nav-link')) {
         e.preventDefault();
         const url = e.target.getAttribute('href').split('/').pop();
         window.history.pushState(url, '', url);
         main.focus();
-        updateMainContent(url);
+        updatePage(url);
         updateWindowTitle(url);
     }
 }
 
+/**
+ * Updates the page. Called when:
+ * 1) the browser back/forward button is clicked.
+ */
 function popstate(e) {
-    updateMainContent(e.state)
+    updatePage(e.state)
 }
