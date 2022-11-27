0e543c7da5c28da4b566a5fd34cdf42c25e54e94
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-11-27 14:31:45 +0200

    move languages data in separate file

--- src/js/script.js
+++ src/js/script.js
@@ -1,24 +1,4 @@
-const states = {
-    'index.html': {
-        'name': 'Languages',
-        'description': 'A programming language is any set of rules that converts strings, or graphical program elements in the case of visual programming languages, to various kinds of machine code output. Programming languages are one kind of computer language, and are used in computer programming to implement algorithms.'
-    },
-    'javascript.html': {
-        'name': 'JavaScript',
-        'logo': 'img/javascript.png',
-        'description': 'JavaScript often abbreviated JS, is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS. Over 97% of websites use JavaScript on the client side for web page behavior, often incorporating third-party libraries. All major web browsers have a dedicated JavaScript engine to execute the code on users\' devices.'
-    },
-    'python.html': {
-        'name': 'Python',
-        'logo': 'img/python.png',
-        'description': 'Python is a high-level, interpreted, general-purpose programming language. Its design philosophy emphasizes code readability with the use of significant indentation. Its language constructs and object-oriented approach aim to help programmers write clear, logical code for small- and large-scale projects.'
-    },
-    'c++.html': {
-        'name': 'C++',
-        'logo': 'img/c++.png',
-        'description': 'C++ is a general-purpose programming language created by Bjarne Stroustrup as an extension of the C programming language, or "C with Classes". The language has expanded significantly over time, and modern C++ now has object-oriented, generic, and functional features in addition to facilities for low-level memory manipulation.'
-    }
-};
+import { languagesData } from './data.js'
 
 let main, mainText, header, h1;
 const nav = document.querySelector('.nav');
@@ -69,8 +49,8 @@ function createMainContent() {
  * 3) the browser back/forward button is clicked.
  */
 function updatePage(url) {
-    h1.textContent = states[url].name;
-    mainText.textContent = states[url].description;
+    h1.textContent = languagesData[url].name;
+    mainText.textContent = languagesData[url].description;
     updateLanguageLogo(url);
     updateSelectedNavLink(url);
 }
@@ -128,9 +108,9 @@ function updateSelectedNavLink(url) {
  */
 function updateWindowTitle(url) {
     if (url === 'index.html') {
-        document.title = states['index.html'].name;
+        document.title = languagesData['index.html'].name;
     } else {
-        document.title = states['index.html'].name + ' - ' + states[url].name;
+        document.title = languagesData['index.html'].name + ' - ' + languagesData[url].name;
     }
 }
 
